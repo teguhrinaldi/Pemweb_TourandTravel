0b1639ed3df8b15bd2119954b7254e8fe332db06
razziqdhav <razziqdhaf@gmail.com>
2025-01-12 21:24:14 +0700

    razziq(#18)

--- resources/views/components/landingPage/best.blade.php
+++ resources/views/components/landingPage/best.blade.php
@@ -1,17 +1,20 @@
 <div class="wrapper">
     <section class="blog-section container section">
         <div class="blog-sec-container">
+            <!-- Section Intro -->
             <div class="blog-sec-intro">
                 <h2 data-aos="fade-up" data-aos-duration="2000" class="blog-sec-title">
-                    Our Best Blog?
+                    Our Best Blog
                 </h2>
                 <p data-aos="fade-up" data-aos-duration="2500">
                     An insight to the incredible experience in the world.
                 </p>
             </div>
 
+            <!-- Main Blog Content -->
             <div class="blog-main-container grid">
                 @foreach ($blogInformation as $post)
+                    <!-- Single Blog Post -->
                     <div data-aos="fade-up" data-aos-duration="3000" class="blog-single-post grid">
                         <div class="blog-img-div">
                             <img src="{{ $post['postImage'][0] }}" alt="{{ $post['title'] }}">
@@ -20,21 +23,14 @@
                             <h3>{{ $post['title'] }}</h3>
                             <p>{{ $post['desc'] }}</p>
                         </div>
-                        <div class="blog-read-more" onclick="location.href='{{ url('bestblog/' . $post['location']) }}'">
+                        <!-- Read More Button -->
+                        <div class="blog-read-more" onclick="location.href='{{ url('blog/' . $post['location']) }}'">
                             <span>Read More</span>
                             <i class="icon"></i>
                         </div>
                     </div>
-                    <div class="postDetails">
-                        <h3>{{ $post['title'] }}</h3>
-                        <p>{{ $post['desc'] }}</p>
-                    </div>
-                    <div class="readmore" onclick="location.href='{{ url('blog/' . $post['location']) }}'">
-                        <span>Read More</span>
-                        <i class="icon"></i>
-                    </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
         </div>
     </section>
 </div>
