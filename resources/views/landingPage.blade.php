e225e596b5dca421fe79470277eec32be69cbf3e
imyoursatan <teguhrinaldi23@gmail.com>
2025-01-13 14:38:52 +0700

    resolve conflict

--- resources/views/landingPage.blade.php
+++ resources/views/landingPage.blade.php
@@ -18,33 +18,13 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
 </head>
 <body>
-    <!-- Panggil Navbar -->
     <x-landingPage.navbar />
-
-    <!-- Panggil Jumbotron -->
-    <x-landingPage.jumbotron />
-
-    <!-- Panggil Content (untuk konten popular destination di masa depan) -->
-    <x-landingPage.content :popularDestination="$popularDestination" />
-
-    <!-- Tambahkan JavaScript jika diperlukan -->
-    <!-- Panggil Offers -->
-    <x-landingPage.offers :offerInformation="$offerInformation" /> 
-    
+    <x-landingPage.jumbotron />   
+    <x-landingPage.content :popularDestination="$popularDestination" />   
+    <x-landingPage.offers :offerInformation="$offerInformation" />  
     <x-landingPage.about />
-
-
-    <x-landingPage.best :blogInformation="$blogInformation" />
-
-   
-    
-
     <x-landingPage.best :blogInformation="$blogInformation" />
     <x-landingPage.footer />
-    <x-landingPage.top />
-    <x-landingPage.blog />
-
-
     <!-- Tambahkan JavaScript jika diperlukan -->
     <script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"></script>
     <script>
